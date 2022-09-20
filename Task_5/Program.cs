254bc1d5fb76af9082d332a051cc6d80f7bf5e92
Vadim Sadovnikov <vadimsadovnikov007@mail.ru>
2022-09-20 23:10:20 +1000

    Added Task 5 solution

---
+++ Task_5/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int number = int.Parse (Console.ReadLine());
+
+int start_number = Math.Abs(number) * -1;
+
+while (start_number < number + 1)
+{
+    Console.WriteLine (start_number);
+    start_number = start_number + 1;
+}
