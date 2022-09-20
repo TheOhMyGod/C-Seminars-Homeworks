146fd2427ff8e194e3ac126c8343113b7d0ad9b7
Vadim Sadovnikov <vadimsadovnikov007@mail.ru>
2022-09-20 23:48:22 +1000

    Added Task 6 HW solution

---
+++ Task_6_HW/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число: ");
+
+int number = Convert.ToInt32 (Console.ReadLine());
+
+int number_remainder = number % 2;
+
+if (number_remainder == 0)
+{
+    Console.WriteLine ($"{number} - четное число!");
+}
+
+else
+{
+    Console.WriteLine ($"{number} - нечетное число!");
+}
