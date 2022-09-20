6fee23849709369aab971628190d01cb1668332e
Vadim Sadovnikov <vadimsadovnikov007@mail.ru>
2022-09-20 23:40:39 +1000

    Added Task 4 HW solution

---
+++ Task_4_HW/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write ("Введите число 1: ");
+int number_A = Convert.ToInt32 (Console.ReadLine());
+
+Console.Write ("Введите число 2: ");
+int number_B = Convert.ToInt32 (Console.ReadLine());
+
+Console.Write ("Введите число 3: ");
+int number_C = Convert.ToInt32 (Console.ReadLine());
+
+int max = number_A;
+
+if (number_A > number_B) max = number_A;
+if (number_B > number_C) max = number_B;
+if (number_C > number_A) max = number_C;
+
+Console.WriteLine ($"Наибольшее число - {max}");
