a72f01cee82603a326535984f517fcfe397d0558
Vadim Sadovnikov <vadimsadovnikov007@mail.ru>
2022-09-20 22:57:08 +1000

    Added Task 3 soluiton

---
+++ Task_3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите номер дня недели: ");
+int number = int.Parse (Console.ReadLine());
+
+if (number == 1) Console.WriteLine("Первый день недели - Понедельник.");
+if (number == 2) Console.WriteLine("Второй день недели - Вторник.");
+if (number == 3) Console.WriteLine("Третий день недели - Среда.");
+if (number == 4) Console.WriteLine("Четвертый день недели - Четверг.");
+if (number == 5) Console.WriteLine("Пятый день недели - Пятница.");
+if (number == 6) Console.WriteLine("Шестой день недели - Суббота.");
+if (number == 7) Console.WriteLine("Седьмой день недели - Воскресенье.");
+if (number > 7) Console.WriteLine("В неделе семь дней!");
