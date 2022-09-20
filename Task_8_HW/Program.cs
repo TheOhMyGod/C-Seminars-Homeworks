672d1cea0dd26f1c62832b64f3d75a2c7e44398a
Vadim Sadovnikov <vadimsadovnikov007@mail.ru>
2022-09-21 00:05:06 +1000

    Added Task 8 HW solution

---
+++ Task_8_HW/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+
+int number = Convert.ToInt32 (Console.ReadLine());
+
+int start_number = 1;
+
+while (start_number < number + 1)
+{
+    int number_remainder = start_number % 2;
+    if (number_remainder == 0) Console.WriteLine (start_number);
+    start_number = start_number + 1;
+}
