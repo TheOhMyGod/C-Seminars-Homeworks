6e19562e765a64bc4b05d218b8dd61c5b7db38e4
Vadim Sadovnikov <vadimsadovnikov007@mail.ru>
2022-09-23 23:14:09 +1000

    Add Task 9 solution

---
+++ Task_9/Program.cs
@@ -0,0 +1,9 @@
+﻿int x = new Random().Next(10, 100);
+int x1 = x / 10;
+int x2 = x % 10;
+
+Console.WriteLine($"Сгенерированное случайное число - {x}");
+
+if (x1>x2) Console.WriteLine($"Первая цифра - {x1} больше");
+else if (x2>x1)Console.WriteLine($"Вторая цифра - {x2} больше");
+else Console.WriteLine("Это паллиндром!");
