93a5be116d2d81fa863ce7c0d5a7d4eaa7898a5e
Vadim Sadovnikov <vadimsadovnikov007@mail.ru>
2022-09-27 21:48:46 +1000

    Added Task 17 solution

---
+++ Task_17/Program.cs
@@ -0,0 +1,34 @@
+﻿// Определение четверти точки
+int GetPointArea(int x, int y)
+{
+    if (x > 0 && y > 0)
+    {
+        return 1;
+    }
+
+    if (x < 0 && y > 0)
+    {
+        return 2;
+    }
+
+    if (x < 0 && y < 0)
+    {
+        return 3;
+    }
+
+    if (x > 0 && y < 0)
+    {
+        return 4;
+    }
+
+    return 0;
+}
+
+Console.Write("Введите X: ");
+int x = int.Parse(Console.ReadLine());
+
+Console.Write("Введите Y: ");
+int y = int.Parse(Console.ReadLine());
+
+int areaNum = GetPointArea(x, y);
+Console.WriteLine($"Четерть заданной точки - {areaNum}");
