e222882d37e5dc4d156589d9fe40558e0f4e8bc3
Vadim Sadovnikov <vadimsadovnikov007@mail.ru>
2022-09-27 22:21:56 +1000

    Added Task 18 solution

---
+++ Task_18/Program.cs
@@ -0,0 +1,31 @@
+﻿// Определение диапазона по заданной четверти
+string AreaNums(int x)
+{
+    if (x == 1)
+    {
+        return "В данной четверти всегда - x > 0, y > 0";
+    }
+
+    if (x == 2)
+    {
+        return "В данной четверти всегда - x < 0, y > 0";
+    }
+
+    if (x == 3)
+    {
+        return "В данной четверти всегда - x < 0, y < 0";
+    }
+
+    if (x == 4)
+    {
+        return "В данной четверти всегда - x > 0, y < 0";
+    }
+
+    return "Нет такой четверти!";
+}
+
+Console.Write("Введите название четверти: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+string result = AreaNums(x);
+Console.Write(result);
