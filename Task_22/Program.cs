3890b7abe13cca690f2f5b0f6d35d185d7736680
Vadim Sadovnikov <vadimsadovnikov007@mail.ru>
2022-10-01 14:33:36 +1000

    Added Task 22 solution

---
+++ Task_22/Program.cs
@@ -0,0 +1,22 @@
+﻿// Метод принимает число и создает массив с квадратами от 1 до введенного числа
+double[] GetSquaresNumbers (int number)
+{
+    double[] squaresNumbers = new double[number];
+
+    // Заполнение массива результата
+    for (int i = 0; i < number; i++)
+    {
+        squaresNumbers[i] = Math.Pow(i + 1, 2);
+    }
+
+    return squaresNumbers;
+}
+
+Console.Write ("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+double[] result = GetSquaresNumbers(num);
+for (int i = 0; i < result.Length; i++)
+{
+    Console.WriteLine(result[i]);
+}
