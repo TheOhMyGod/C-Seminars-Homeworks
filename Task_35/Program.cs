87fc95835668112b3f8ac944cb2dd47fb4c4aa7d
Vadim Sadovnikov <vadimsadovnikov007@mail.ru>
2022-10-01 20:58:33 +1000

    Added Task 35 solution

---
+++ Task_35/Program.cs
@@ -0,0 +1,44 @@
+﻿// Заполняет массив случайными числами
+int[] GetArrayFilled(int[] array)
+{
+    var random = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(1, 1000);
+    }
+
+    return array;
+}
+
+// Выводит массив
+void PrintArray (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.WriteLine();
+}
+
+// Считает числа от 10 до 99 включительно
+int MatchCounter (int[] array)
+{
+    int counter = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= 10 && array[i] < 100)
+        {
+            counter++;
+        }
+    }
+    return counter;
+}
+
+int[] array = new int[123];
+
+int[] arrayFilled = GetArrayFilled(array);
+int counter = MatchCounter(arrayFilled);
+
+PrintArray(arrayFilled);
+
+Console.WriteLine($"В сгенерированном массиве {counter} чисел от 10 до 99.");
