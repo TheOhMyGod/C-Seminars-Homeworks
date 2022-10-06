945feae0fd6115a1b6a0ecc946aaeb870c19a556
Vadim Sadovnikov <vadimsadovnikov007@mail.ru>
2022-10-07 02:05:59 +1000

    Added Task 42 solution

---
+++ Task_42/Program.cs
@@ -0,0 +1,19 @@
+﻿int GetTensToTwos (int number)
+{
+    int numberTens = 0;
+    int rem = 0;
+    int x = 1;
+    while (number != 0)
+    {
+        rem = number % 2;
+        numberTens = numberTens + rem*x;
+        number = number / 2;
+        x = x * 10;
+    }
+    return numberTens;
+}
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine(GetTensToTwos(number));
---
+++ hsh/Program.cs
@@ -0,0 +1,10 @@
+﻿int number = 2;
+int d10 = 1;
+int result = 0;
+while (number != 0)
+{
+    result = result + number % 2 * d10;
+    number = number / 2;
+    d10 = d10 * 10;
+}
+Console.WriteLine(result);
