e73fe0ac5e3f76d2703113d7fa47891432362036
Vadim Sadovnikov <vadimsadovnikov007@mail.ru>
2022-10-07 20:20:42 +1000

    Added Task 51 solution

---
+++ Task_51/Program.cs
@@ -0,0 +1,56 @@
+﻿int[,] FillArray (int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = new Random().Next(1,10);
+        }
+    }
+    return array;
+}
+
+void PrintArray (int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int GetSumOfPrimeLine (int[,] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i == j)
+            {
+                sum = sum+array[i,j];
+            }
+        }
+    }
+    return sum;
+}
+
+Console.Write("Введите количество строк M: ");
+int m = int.Parse(Console.ReadLine());
+
+Console.Write("Введите количество строк N: ");
+int n = int.Parse(Console.ReadLine());
+
+int[,] array = new int[m,n];
+
+FillArray(array);
+
+PrintArray(array);
+Console.WriteLine();
+
+Console.WriteLine($"Сумма чисел находящихся на главной диагонали равна " + GetSumOfPrimeLine(array));
+
+Console.ReadKey();
