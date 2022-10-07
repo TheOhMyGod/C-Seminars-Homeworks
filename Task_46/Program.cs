19c163c0c98fac28b9ce21143adce37f0048d26b
Vadim Sadovnikov <vadimsadovnikov007@mail.ru>
2022-10-07 18:54:27 +1000

    Added Task 46 solution

---
+++ Task_46/Program.cs
@@ -0,0 +1,53 @@
+﻿// Размерность массива (кол-во строк)
+int m = 0;
+
+// Размерность массива (кол-во строк)
+int n = 0;
+
+Console.Write("Введите кол-во строк M: ");
+bool parseMIsOk = int.TryParse(Console.ReadLine(), out int numberM);
+if (!parseMIsOk)
+{
+    Console.WriteLine("Введено значение некоректного формата (M)");
+    return;
+}
+else
+{
+    m = numberM;
+}
+
+
+Console.Write("Введите кол-во столбцов N: ");
+bool parseNIsOk = int.TryParse(Console.ReadLine(), out int numberN);
+if (!parseNIsOk)
+{
+    Console.WriteLine("Введено значение некоректного формата (N)");
+    return;
+}
+else
+{
+    n = numberN;
+}
+
+int[,] array = new int[m, n];
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array[i, j] = new Random().Next(1, 10);
+//       Console.Write($"{array[i,j]} ");
+    }
+//    Console.WriteLine();
+}
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write($"{array[i,j]} ");
+    }
+    Console.WriteLine();
+}
+
+Console.ReadKey();
