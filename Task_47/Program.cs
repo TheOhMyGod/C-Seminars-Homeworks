0d900f644eff034959fe9ddad5d944552ef47bba
Vadim Sadovnikov <vadimsadovnikov007@mail.ru>
2022-10-07 18:54:41 +1000

    Added Task 47 solution

---
+++ Task_47/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите количество строк M:");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов N:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[m,n];
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array[i,j] = i + j;
+        Console.Write($"{array[i,j]} ");
+    }
+    Console.WriteLine();
+}
+
+Console.ReadKey();
