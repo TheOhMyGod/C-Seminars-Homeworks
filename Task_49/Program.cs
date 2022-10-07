2bc558df9d16d0594e35897403bf8c2a48c0b0a6
Vadim Sadovnikov <vadimsadovnikov007@mail.ru>
2022-10-07 20:12:10 +1000

    Added Task 49 solution

---
+++ Task_49/Program.cs
@@ -0,0 +1,55 @@
+﻿int[,] GetEvenElementsDoubled(int[,] array)
+{
+    for (int i = 2; i < array.GetLength(0); i++)
+    {
+        for (int j = 2; j < array.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                array[i, j] = array[i, j] * array[i, j];
+            }
+        }
+    }
+    return array;
+}
+
+int[,] FillArray (int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = new Random().Next(1,10);
+        }
+    }
+    return array;
+}
+
+void PrintArray (int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите количество строк M: ");
+int m = int.Parse(Console.ReadLine());
+
+Console.Write("Введите количество строк N: ");
+int n = int.Parse(Console.ReadLine());
+
+int[,] array = new int[m,n];
+
+FillArray(array);
+
+PrintArray(array);
+
+GetEvenElementsDoubled(array);
+Console.WriteLine();
+
+PrintArray(array);
