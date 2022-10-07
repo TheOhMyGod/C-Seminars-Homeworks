bd1920ce7fb0567898f54a50a72afceb9822b0ef
Vadim Sadovnikov <vadimsadovnikov007@mail.ru>
2022-10-07 20:53:48 +1000

    Added corections to 3 and 5 lines (Changed 2 to 0 in i equals 0)

--- Task_49/Program.cs
+++ Task_49/Program.cs
@@ -1,8 +1,8 @@
 ﻿int[,] GetEvenElementsDoubled(int[,] array)
 {
-    for (int i = 2; i < array.GetLength(0); i++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        for (int j = 2; j < array.GetLength(1); j++)
+        for (int j = 0; j < array.GetLength(1); j++)
         {
             if (i % 2 == 0 && j % 2 == 0)
             {
