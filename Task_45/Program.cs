e40b424735a8314897eea24d152d4832b536a027
Vadim Sadovnikov <vadimsadovnikov007@mail.ru>
2022-10-07 15:21:49 +1000

    Added Task 45 solution

---
+++ Task_45/Program.cs
@@ -0,0 +1,46 @@
+﻿// Заполняет массив случайными числами
+int[] FillArray (int[] array)
+{
+    var random = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(1, 10);
+    }
+
+    return array;
+}
+
+// Выводит массив
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] GetArraysCopy(int[] array)
+{
+    int[] arrayCopy = new int[array.Length];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        arrayCopy[i] = array[i];
+    }
+
+    return arrayCopy;
+}
+
+
+Console.Write("Введите размер массива: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[number];
+
+Console.WriteLine("Сгенерированный массив: ");
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine("Скопированный массив: ");
+PrintArray(GetArraysCopy(array));
